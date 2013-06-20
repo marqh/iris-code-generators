e2fd1d57f5087691e88d710ad281aec1177ba303
marqh <markh@scitools.org.uk>
2013-06-20 11:42:08 +0000

    tools to generate standard names and stash/fc to std name dictionary

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 iris-code-generators
 ====================
+
+This is a repository of tools which manage automated code generation for Iris.
+
+tools/ contains code generation scripts and supporting modules.
---
+++ tools/translator/__init__.py
@@ -0,0 +1,16 @@
+# (C) British Crown Copyright 2013, Met Office
+#
+# This file is part of iris-code-generators.
+#
+# iris-code-generators is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# iris-code-generators is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with iris-code-generators. If not, see <http://www.gnu.org/licenses/>.
